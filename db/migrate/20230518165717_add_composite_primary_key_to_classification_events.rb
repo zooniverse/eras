26b4fda64d7dd2433f55ce8977e31f578622196d
yuenmichelle1 <michelle.dano.yuen@gmail.com>
2023-05-22 16:22:45 -0500

    Update 20230518165717_add_composite_primary_key_to_classification_events.rb

--- db/migrate/20230518165717_add_composite_primary_key_to_classification_events.rb
+++ db/migrate/20230518165717_add_composite_primary_key_to_classification_events.rb
@@ -1,4 +1,4 @@
-class AddCompositPrimaryKeyToClassificationEvents < ActiveRecord::Migration[7.0]
+class AddCompositePrimaryKeyToClassificationEvents < ActiveRecord::Migration[7.0]
   def change
     execute <<~SQL
       ALTER TABLE classification_events ADD PRIMARY KEY(classification_id, event_time);
