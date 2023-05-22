9777fcbe1adf599b9f9baef2f38de8a1a51fb35d
yuenmichelle1 <michelle.dano.yuen@gmail.com>
2023-05-22 16:14:49 -0500

    install rspec and remove test directory that comes with rails in favor of rspec.
    Clean up database.yml initial comments that comes with rails scaffold.
    Clean up gemfile to include comments on gems that we do need.

--- test/models/classification_event_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class ClassificationEventTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/models/classification_user_group_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class ClassificationUserGroupTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
