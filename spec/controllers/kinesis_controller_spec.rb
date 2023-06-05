d483c65ce513a142b9de059ffe7c841404260af3
yuenmichelle1 <michelle.dano.yuen@gmail.com>
2023-06-05 17:26:06 -0500

    create kinesiscontroller spec and update payload fixtures to look like payload. update create_spec due to fix of readability of payload fixtures to send in payload as params of operation (like { payload : [ classifictaion/comment_payload] }

---
+++ spec/controllers/kinesis_controller_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe KinesisController do
+  describe "POST create" do
+    it 'processes the stream events' do
+      post :create, params: { payload: [JSON.load(File.read(Rails.root.join("spec/fixtures/example_kinesis_classification_payload.json")))] }, as: :json
+      expect(response.status).to eq(204)
+    end
+  end
+end
--- spec/operations/kinesis/create_spec.rb
+++ spec/operations/kinesis/create_spec.rb
@@ -1,16 +1,16 @@
 # frozen_string_literal: true
 
-require "rails_helper" 
+require 'rails_helper'
 
 RSpec.describe Kinesis::Create do
-  context 'payload has talk comment' do
+  describe 'comment_event creation' do
     let(:payload) { JSON.parse(File.read(Rails.root.join('spec/fixtures/example_kinesis_comment_payload.json'))) }
-    let(:operation) { described_class.with_options(payload) }
+    let(:operation) { described_class.with_options({ payload: [payload] }) }
 
-    it 'creates comment_event' do
+    it 'creates comment_event if payload is from talk' do
       expect { operation.run! }.to change(CommentEvent, :count).from(0).to(1)
       comment_event = CommentEvent.first
-      payload_data = payload['payload'][0]['data']
+      payload_data = payload['data']
       expect(comment_event.comment_id).to eq(payload_data['id'].to_i)
       expect(comment_event.event_time).to eq(payload_data['created_at'])
       expect(comment_event.comment_updated_at).to eq(payload_data['updated_at'])
@@ -19,14 +19,16 @@ RSpec.describe Kinesis::Create do
     end
   end
 
-  context 'payload has classification' do
-    let(:payload) { JSON.parse(File.read(Rails.root.join('spec/fixtures/example_kinesis_classification_payload.json'))) }
-    let(:operation) { described_class.with_options(payload) }
-    
-    it 'creates classification_event' do
+  describe 'classification_event creation' do
+    let(:payload) do
+      JSON.parse(File.read(Rails.root.join('spec/fixtures/example_kinesis_classification_payload.json')))
+    end
+    let(:operation) { described_class.with_options({ payload: [payload] }) }
+
+    it 'creates classification_event if payload is from panoptes' do
       expect { operation.run! }.to change(ClassificationEvent, :count).from(0).to(1)
       classification_event =  ClassificationEvent.first
-      payload_data = payload['payload'][0]['data']
+      payload_data = payload['data']
       expect(classification_event.classification_id).to eq(payload_data['id'].to_i)
       expect(classification_event.event_time).to eq(payload_data['created_at'])
       expect(classification_event.classification_updated_at).to eq(payload_data['updated_at'])
@@ -35,7 +37,7 @@ RSpec.describe Kinesis::Create do
     it 'sets the classification_event relation ids from payload links attribute' do
       operation.run!
       classification_event = ClassificationEvent.first
-      payload_data = payload['payload'][0]['data']
+      payload_data = payload['data']
       payload_links = payload_data['links']
       expect(classification_event.project_id).to eq(payload_links['project'].to_i)
       expect(classification_event.workflow_id).to eq(payload_links['workflow'].to_i)
@@ -45,7 +47,7 @@ RSpec.describe Kinesis::Create do
     it 'sets the started_at and finished_at from metadata' do
       operation.run!
       classification_event = ClassificationEvent.first
-      payload_data = payload['payload'][0]['data']
+      payload_data = payload['data']
       payload_metadata = payload_data['metadata']
       started_at = Time.parse(payload_metadata['started_at'])
       finished_at = Time.parse(payload_metadata['finished_at'])
@@ -54,7 +56,7 @@ RSpec.describe Kinesis::Create do
     end
 
     it 'sets started_at time to nil if there is no started_at in metadata' do
-      payload_data = payload['payload'][0]['data']
+      payload_data = payload['data']
       payload_metadata = payload_data['metadata']
       payload_metadata.delete('started_at')
       operation.run!
@@ -63,7 +65,7 @@ RSpec.describe Kinesis::Create do
     end
 
     it 'sets finished_at to nil if there is no finished_at in metadata' do
-      payload_data = payload['payload'][0]['data']
+      payload_data = payload['data']
       payload_metadata = payload_data['metadata']
       payload_metadata.delete('finished_at')
       operation.run!
@@ -75,7 +77,7 @@ RSpec.describe Kinesis::Create do
       it 'sets session time to finished_at - started_at if within session limit' do
         operation.run!
         classification_event = ClassificationEvent.first
-        payload_data = payload['payload'][0]['data']
+        payload_data = payload['data']
         payload_metadata = payload_data['metadata']
         started_at = Time.parse(payload_metadata['started_at'])
         finished_at = Time.parse(payload_metadata['finished_at'])
@@ -83,7 +85,7 @@ RSpec.describe Kinesis::Create do
       end
 
       it 'caps session time if finished_at - started_at is over session limit' do
-        payload_data = payload['payload'][0]['data']
+        payload_data = payload['data']
         payload_metadata = payload_data['metadata']
         started_at = Time.parse(payload_metadata['started_at'])
         finished_at = started_at + Kinesis::Create::SESSION_LIMIT + 1
@@ -94,9 +96,24 @@ RSpec.describe Kinesis::Create do
       end
 
       it 'sets session time to 0 if finished_at - started_at is negative' do
+        payload_data = payload['data']
+        payload_metadata = payload_data['metadata']
+        started_at = Time.parse(payload_metadata['started_at'])
+        finished_at = started_at - 1
+        payload_metadata['finished_at'] = finished_at.strftime('%Y-%m-%dT%H:%M:%S%z')
+        operation.run!
+        classification_event = ClassificationEvent.first
+        expect(classification_event.session_time).to eq(0)
       end
 
       it 'sets session time to 0 if finished_at - started_at is nil' do
+        payload_data = payload['data']
+        payload_metadata = payload_data['metadata']
+        payload_metadata.delete('started_at')
+        payload_metadata.delete('finished_at')
+        operation.run!
+        classification_event = ClassificationEvent.first
+        expect(classification_event.session_time).to eq(0)
       end
     end
 
