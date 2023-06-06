826e42c7c0ba94080b44f4267fba9e7a06be0c7e
yuenmichelle1 <michelle.dano.yuen@gmail.com>
2023-06-06 15:09:40 -0500

    Update kinesis_spec.rb

--- spec/requests/kinesis_spec.rb
+++ spec/requests/kinesis_spec.rb
@@ -1,5 +1,12 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe "Kinesis", type: :request do
-  
+RSpec.describe 'Kinesis', type: :request do
+  it 'processes the stream events' do
+    post '/kinesis', headers: { 'CONTENT_TYPE' => 'application/json' },
+                     params: "{\"payload\": [#{File.read(Rails.root.join('spec/fixtures/example_kinesis_comment_payload.json'))}]}"
+    expect(response.status).to eq(204)
+    expect(CommentEvent.count).to eq(1)
+  end
 end
