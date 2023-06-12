80f09d1df398e812a65b0d52abbc53aa58991003
yuenmichelle1 <michelle.dano.yuen@gmail.com>
2023-06-12 16:38:28 -0500

    add spec for event creation

--- spec/models/kinesis_stream_spec.rb
+++ spec/models/kinesis_stream_spec.rb
@@ -10,6 +10,11 @@ RSpec.describe KinesisStream do
   end
 
   describe 'receive' do
+    it 'processes each item in the payload' do
+      expect(StreamEvents).to receive(:from).with(comment_payload).ordered.and_return(double(process: true))
+      expect(StreamEvents).to receive(:from).with(classification_payload).ordered.and_return(double(process: true))
+      kinesis_stream.receive([comment_payload, classification_payload])
+    end
     context 'payload has a comment' do
       it 'adds to comment_events array' do
         kinesis_stream.receive([comment_payload])
@@ -33,6 +38,28 @@ RSpec.describe KinesisStream do
         links['user'] = nil
         kinesis_stream.receive([classification_payload])
         expect(kinesis_stream.instance_variable_get(:@classification_user_groups).length).to be_zero
+      end
+    end
+  end
+
+  describe 'create_events' do
+    context 'payload has comments' do
+      it 'creates comment_events' do
+        expect { kinesis_stream.create_events([comment_payload]) }.to change(CommentEvent, :count).from(0).to(1)
+      end
+    end
+
+    context 'payload has classifications' do
+      it 'creates classification_event' do
+        expect do
+          kinesis_stream.create_events([classification_payload])
+        end.to change(ClassificationEvent, :count).from(0).to(1)
+      end
+
+      it 'creates classification_user_groups if classifying user belongs to multiple user groups' do
+        expect do
+          kinesis_stream.create_events([classification_payload])
+        end.to change(ClassificationUserGroup, :count).from(0).to(2)
       end
     end
   end
