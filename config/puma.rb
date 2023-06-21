f4e474d81ae0fdac4814a3d309773554318b810f
Zach Wolfenbarger <zach.wolfenbarger@gmail.com>
2023-06-21 16:49:34 -0500

    Set port based on env

--- config/puma.rb
+++ config/puma.rb
@@ -15,11 +15,11 @@ worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
-port ENV.fetch("PORT") { 3000 }
-
-# Specifies the `environment` that Puma will run in.
-#
-environment ENV.fetch("RAILS_ENV") { "development" }
+dev_env = 'development'
+rails_env = ENV.fetch('RAILS_ENV') || dev_env
+port = rails_env == dev_env ? 3000 : 80
+environment rails_env
+bind "tcp://0.0.0.0:#{port}"
 
 # Specifies the `pidfile` that Puma will use.
 pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
