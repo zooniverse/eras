8e59c3cd1a1dddd128de1e00e2520da192d2906c
yuenmichelle1 <michelle.dano.yuen@gmail.com>
2023-06-29 10:32:55 -0500

    add classification events factory and  count classifications query spec

--- spec/queries/count_classifications_spec.rb
+++ spec/queries/count_classifications_spec.rb
@@ -38,5 +38,55 @@ RSpec.describe CountClassifications do
 
   describe '#call' do
     it_behaves_like 'is filterable'
+    let!(:classification_event) { create(:classification_event) }
+    let!(:diff_workflow_event) { create(:event_with_diff_workflow) }
+    let!(:diff_project_event) { create(:event_with_diff_project) }
+    let!(:diff_time_event) { create(:event_created_yesterday) }
+
+    it 'returns counts of all events when no params given' do
+      counts = count_classifications.call(params)
+      # because default is bucket by year and all data created in the same year, we expect counts to look something like
+      # [<ClassificationCounts::DailyClassificationCount period: 01-01-2023, count: 4>]
+      current_year = Date.today.year
+      expect(counts.length).to eq(1)
+      expect(counts[0].count).to eq(4)
+      expect(counts[0].period).to eq("01-01-#{current_year}")
+    end
+
+    it 'returns counts bucketed by given period' do
+      params[:period] = 'day'
+      counts = count_classifications.call(params)
+      expect(counts.length).to eq(2)
+      expect(counts[0].count).to eq(1)
+      expect(counts[0].period).to eq((Date.today - 1).to_s)
+      expect(counts[1].count).to eq(3)
+      expect(counts[1].period).to eq(Date.today.to_s)
+    end
+
+    it 'returns counts of events with given workflow' do
+      workflow_id = diff_workflow_event.workflow_id
+      params[:workflow_id] = workflow_id.to_s
+      counts = count_classifications.call(params)
+      expect(counts.length).to eq(1)
+      expect(counts[0].count).to eq(1)
+    end
+
+    it 'returns counts of events with given project' do
+      project_id = diff_project_event.project_id
+      params[:project_id] = project_id.to_s
+      counts = count_classifications.call(params)
+      expect(counts.length).to eq(1)
+      expect(counts[0].count).to eq(1)
+    end
+
+    it 'returns counts of events within given date range' do
+      last_week = Date.today - 7
+      today = Date.today
+      params[:start_date] = last_week.to_s
+      params[:end_date] = today.to_s
+      counts = count_classifications.call(params)
+      expect(counts.length).to eq(1)
+      expect(counts[0].count).to eq(1)
+    end
   end
 end
