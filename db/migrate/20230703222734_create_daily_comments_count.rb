5a671ab944a9d0d1581d8caf18b2c83033f79fbd
yuenmichelle1 <michelle.dano.yuen@gmail.com>
2023-07-06 12:39:51 -0500

    adding frozen string literal true to migration files per hound sniffs

--- db/migrate/20230703222734_create_daily_comments_count.rb
+++ db/migrate/20230703222734_create_daily_comments_count.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class CreateDailyCommentsCount < ActiveRecord::Migration[7.0]
   # we have to disable the migration transaction because creating materialized views within it is not allowed.
   disable_ddl_transaction!
