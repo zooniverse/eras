0f2f55ad02f585323cc9cc0a6fcec94bb5c11d48
yuenmichelle1 <michelle.dano.yuen@gmail.com>
2023-07-17 14:22:19 -0500

    Update user_classification_counts_serializer.rb

--- app/serializers/user_classification_counts_serializer.rb
+++ app/serializers/user_classification_counts_serializer.rb
@@ -13,17 +13,35 @@ class UserClassificationCountsSerializer
     show_time_spent = serializer_options[:show_time_spent]
     num_top_projects_to_show = serializer_options[:top_project_contributions]
     total_count = user_classification_counts.sum(&:count).to_i
-    total_time_spent = user_classification_counts.sum(&:session_time).to_f if show_time_spent
-
     response = { total_count: }
-    response[:time_spent] = total_time_spent if show_time_spent
+    calculate_time_spent(user_classification_counts, response) if show_time_spent
     show_proj_contributions(response, num_top_projects_to_show) if num_top_projects_to_show
-    response[:data] = user_classification_counts if period
-    puts "MDY114 USER_CLASSIFICATION COUNTS #{user_classification_counts.group_by { |el| el[:period] }}"
+    response[:data] = response_data(user_classification_counts, num_top_projects_to_show, show_time_spent) if period
     response
   end
 
   private
+
+  def calculate_time_spent(counts, response)
+    total_time_spent = counts.sum(&:session_time).to_f
+    response[:time_spent] = total_time_spent
+  end
+
+  def response_data(user_counts, num_top_projects, show_time_spent)
+    data = user_counts
+    # when calculating top projects, our records returned from query will be counts per user per project
+    # eg.  { period: '01-01-2020', count: 38, project_id: 1 }, { period: '01-01-2020', count: 40, project_id: 2}
+    # vs. Our wanted response format which is counts grouped by bucketed time. { period: '01-02-2020', count: 78 }
+    if num_top_projects
+      counts_grouped_by_period = user_counts.group_by { |user_proj_class_count| user_proj_class_count[:period] }.transform_values do |c|
+        val = { count: c.sum(&:count) }
+        val[:session_time] = c.sum(&:session_time) if show_time_spent
+        val
+      end
+      data = counts_grouped_by_period.map { |period, v| { period: }.merge(v) }
+    end
+    data
+  end
 
   def show_proj_contributions(response, num_top_projects_to_show)
     response[:unique_projects_contributions] = unique_projects_count
@@ -38,6 +56,6 @@ class UserClassificationCountsSerializer
     project_contributions = @user_classification_counts.group_by(&:project_id).transform_values do |counts|
       counts.sum(&:count)
     end
-    project_contributions.map { |k, v| { project_id: k, count: v } }.sort_by { |p| p[:count] }.reverse.first(num_top_projects)
+    project_contributions.map { |project_id, count| { project_id:, count: } }.sort_by { |proj_contribution| proj_contribution[:count] }.reverse.first(num_top_projects)
   end
 end
