90eadd627dfca5ee76d9764284fa09fa91729fce
yuenmichelle1 <michelle.dano.yuen@gmail.com>
2023-07-21 16:47:38 -0500

    remove unneeded scopes in app policies

--- app/policies/application_policy.rb
+++ app/policies/application_policy.rb
@@ -11,19 +11,4 @@ class ApplicationPolicy
   def panoptes_admin?
     user['admin']
   end
-
-  class Scope
-    def initialize(user, scope)
-      @user = user
-      @scope = scope
-    end
-
-    def resolve
-      raise NotImplementedError, "You must define #resolve in #{self.class}"
-    end
-
-    private
-
-    attr_reader :user, :scope
-  end
 end
