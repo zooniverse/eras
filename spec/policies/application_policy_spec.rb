cf1a90bb87a77128d89deb8c4cd9cdd6a9cd26bc
yuenmichelle1 <michelle.dano.yuen@gmail.com>
2023-07-25 09:48:29 -0500

    remove spec that checks if error raised if user nil on application policy to allow more open scope

--- spec/policies/application_policy_spec.rb
+++ spec/policies/application_policy_spec.rb
@@ -22,10 +22,4 @@ RSpec.describe ApplicationPolicy, type: :policy do
       expect(policy.panoptes_admin?).to be true
     end
   end
-
-  context 'without a user' do
-    it 'is not logged in' do
-      expect { ApplicationPolicy.new(nil, records) }.to raise_error Pundit::NotAuthorizedError
-    end
-  end
 end
