351bb56beb6eb34719f120d50dbf0314428397de
yuenmichelle1 <michelle.dano.yuen@gmail.com>
2023-07-25 14:51:40 -0500

    create migrations

--- db/migrate/20230725194157_create_daily_group_workflow_classification_count_and_time.rb
+++ db/migrate/20230725194157_create_daily_group_workflow_classification_count_and_time.rb
@@ -1,19 +1,20 @@
-class CreateDailyGroupClassificationsCountAndTime < ActiveRecord::Migration[7.0]
+class CreateDailyGroupWorkflowClassificationCountAndTime < ActiveRecord::Migration[7.0]
   # we have to disable the migration transaction because creating materialized views within it is not allowed.
   disable_ddl_transaction!
   def change
     execute <<~SQL
-      create materialized view daily_group_classification_count_and_time
+      create materialized view daily_group_classification_count_and_time_per_workflow
       with (
         timescaledb.continuous
       ) as
       select
         time_bucket('1d', event_time) as day,
         user_group_id,
+        workflow_id,
         count(*) as classification_count,
         sum(session_time) as total_session_time
       from classification_user_groups
-      group by day, user_group_id;
+      group by day, user_group_id, workflow_id;
     SQL
   end
 end
