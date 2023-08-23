962afad2e0eff4fe116d0a970a434c92c0926fc2
yuenmichelle1 <michelle.dano.yuen@gmail.com>
2023-08-23 09:45:43 -0500

    Classification user groups migrations and models (#23)
    * add migration daily group counts  and routes
    
    * create migrations
    
    * models associated with continuous aggregates and update error message on user classification count controller
    
    * add migrations for daily group count and time per user per project/workflow
    
    * add models for daily group user per project/workflow counts
    
    * initial logic of user group classifications report
    
    * Update user_group_classification_count_controller.rb
    
    * update migrations with frozen string literal comment and other hound sniffs

---
+++ db/migrate/20230725185947_create_daily_group_classifications_count_and_time.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+class CreateDailyGroupClassificationsCountAndTime < ActiveRecord::Migration[7.0]
+  # we have to disable the migration transaction because creating materialized views within it is not allowed.
+  disable_ddl_transaction!
+  def change
+    execute <<~SQL
+      create materialized view daily_group_classification_count_and_time
+      with (
+        timescaledb.continuous
+      ) as
+      select
+        time_bucket('1d', event_time) as day,
+        user_group_id,
+        count(*) as classification_count,
+        sum(session_time) as total_session_time
+      from classification_user_groups
+      group by day, user_group_id;
+    SQL
+  end
+end
---
+++ db/migrate/20230801163815_create_daily_group_user_project_count_and_time.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+class CreateDailyGroupUserProjectCountAndTime < ActiveRecord::Migration[7.0]
+  # we have to disable the migration transaction because creating materialized views within it is not allowed.
+  disable_ddl_transaction!
+  ## We should note that this view looks very similar to the daily_user_classification_count_and_time_per_project
+  ## (where the columns of that table are day, user_id, classification_count, total_session_time, project_id).
+  ## The only difference between this view and daily_user_classification_count_and_time_per_project is that we are grouping by user_group_id (i.e. user_group_id is a column in this view).
+  ## Even though the views are very similar we cannot query from just daily_user_classification_count_and_time_per_project to get stats info of that user for that group.
+  ## Reason being:
+  ## A) daily_user_classification_count_and_time_per_project does not consider WHEN a user has joined a group.
+  ## (So if we queried for all time for the user group, the user's old classifications [when he/she/they were not part of the group] would be counted towards the user group)
+  ## [Vice versa if a user LEAVES a user group]
+  ## B) On the flip side, we cannot query from just daily_group_classification_count_and_time_per_user_per_prject when querying for just user stats, because not every user belongs to a group.
+  def change
+    execute <<~SQL
+      create materialized view daily_group_classification_count_and_time_per_user_per_project
+      with (
+        timescaledb.continuous
+      ) as
+      select
+        time_bucket('1d', event_time) as day,
+        user_group_id,
+        user_id,
+        project_id,
+        count(*) as classification_count,
+        sum(session_time) as total_session_time
+      from classification_user_groups
+      group by day, user_group_id, user_id, project_id;
+    SQL
+  end
+end
