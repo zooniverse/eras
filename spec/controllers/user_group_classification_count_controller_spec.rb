b8b12913fcc941c8a8e56159bbad93989d2ef139
yuenmichelle1 <michelle.dano.yuen@gmail.com>
2023-08-24 15:11:24 -0500

    update user group controller specs

--- spec/controllers/user_group_classification_count_controller_spec.rb
+++ spec/controllers/user_group_classification_count_controller_spec.rb
@@ -3,10 +3,12 @@
 require 'rails_helper'
 
 RSpec.describe UserGroupClassificationCountController do
+  include AuthenticationHelpers
+
   describe 'GET query' do
     let!(:classification_user_group) { create(:classification_user_group) }
 
-    context 'individual_stats_breakdown is false/not a param' do
+    shared_examples 'shows group aggregate stats' do
       it 'returns total_count, time_spent, active_users, and project_contributions of user group' do
         get :query, params: { id: classification_user_group.user_group_id.to_s }
         expect(response.status).to eq(200)
@@ -16,28 +18,353 @@ RSpec.describe UserGroupClassificationCountController do
         expect(response_body['active_users']).to eq(1)
         expect(response_body['project_contributions'].length).to eq(1)
       end
-
-      it 'does not compute project_contributions when params[:project_id] given' do
-        get :query, params: { id: classification_user_group.user_group_id.to_s, project_id: 2 }
-        expect(response.status).to eq(200)
-        response_body = JSON.parse(response.body)
-        expect(response_body).not_to have_key('project_contributions')
-      end
-
-      it 'does not compute project_contributions when params[:workflow_id] given' do
-        get :query, params: { id: classification_user_group.user_group_id.to_s, workflow_id: 2 }
-        expect(response.status).to eq(200)
-        response_body = JSON.parse(response.body)
-        expect(response_body).not_to have_key('project_contributions')
-      end
     end
 
-    context 'individual_stats_breakdown is true' do
+    shared_examples 'shows group individual stats breakdown' do
       it 'returns group_member_stats_breakdown' do
         get :query, params: { id: classification_user_group.user_group_id.to_s, individual_stats_breakdown: true }
         expect(response.status).to eq(200)
         response_body = JSON.parse(response.body)
         expect(response_body).to have_key('group_member_stats_breakdown')
+      end
+    end
+
+    shared_context 'user_group member' do
+      before(:each) {
+        authenticate_with_membership!(classification_user_group, [membership(classification_user_group)])
+      }
+    end
+
+    shared_context 'user_group admin' do
+      before(:each) {
+        authenticate_with_membership!(classification_user_group, [membership(classification_user_group, 'group_admin')])
+      }
+    end
+
+    shared_context 'zooniverse admin' do
+      before(:each) {
+        authenticate_with_membership!(classification_user_group, [], is_panoptes_admin: true)
+      }
+    end
+
+    shared_context 'user group with stats_visibility' do |stats_visibility|
+      before(:each) {
+        user_groups_url = "/user_groups/#{classification_user_group.user_group_id}"
+        allow(panoptes_application_client).to receive_message_chain(:panoptes, :get).with(user_groups_url).and_return('user_groups' => [user_group(classification_user_group, stats_visibility)])
+      }
+    end
+
+    before(:each) {
+      allow(controller).to receive(:panoptes_application_client).and_return(panoptes_application_client)
+    }
+
+    context 'individual_stats_breakdown is false/not a param' do
+      context 'public_show_all' do
+        include_context 'user group with stats_visibility', 'public_show_all'
+        context 'querying user is not a member' do
+          before(:each) {
+            authenticate_with_membership!(classification_user_group, [])
+          }
+
+          it_behaves_like 'shows group aggregate stats'
+
+          it 'does not compute project_contributions when params[:project_id] given' do
+            get :query, params: { id: classification_user_group.user_group_id.to_s, project_id: 2 }
+            expect(response.status).to eq(200)
+            response_body = JSON.parse(response.body)
+            expect(response_body).not_to have_key('project_contributions')
+          end
+
+          it 'does not compute project_contributions when params[:workflow_id] given' do
+            get :query, params: { id: classification_user_group.user_group_id.to_s, workflow_id: 2 }
+            expect(response.status).to eq(200)
+            response_body = JSON.parse(response.body)
+            expect(response_body).not_to have_key('project_contributions')
+          end
+        end
+
+        context 'querying user is a group member' do
+          include_context 'user_group member'
+          it_behaves_like 'shows group aggregate stats'
+        end
+
+        context 'querying user is a group admin' do
+          include_context 'user_group admin'
+          it_behaves_like 'shows group aggregate stats'
+        end
+
+        context 'querying user is a zooniverse admin' do
+          include_context 'zooniverse admin'
+          it_behaves_like 'shows group aggregate stats'
+        end
+      end
+
+      context 'public_agg_show_ind_if_member' do
+        include_context 'user group with stats_visibility', 'public_agg_show_ind_if_member'
+
+        context 'querying user is not a member' do
+          before(:each) {
+            authenticate_with_membership!(classification_user_group, [])
+          }
+
+          it_behaves_like 'shows group aggregate stats'
+        end
+
+        context 'querying user is a group member' do
+          include_context 'user_group member'
+          it_behaves_like 'shows group aggregate stats'
+        end
+
+        context 'querying user is a group admin' do
+          include_context 'user_group admin'
+          it_behaves_like 'shows group aggregate stats'
+        end
+
+        context 'querying user is a zooniverse admin' do
+          include_context 'zooniverse admin'
+          it_behaves_like 'shows group aggregate stats'
+        end
+      end
+
+      context 'public_agg_only' do
+        include_context 'user group with stats_visibility', 'public_agg_only'
+
+        context 'querying user is not a member' do
+          before(:each) {
+            authenticate_with_membership!(classification_user_group, [])
+          }
+
+          it_behaves_like 'shows group aggregate stats'
+        end
+
+        context 'querying user is a group member' do
+          include_context 'user_group member'
+          it_behaves_like 'shows group aggregate stats'
+        end
+
+        context 'querying user is a group admin' do
+          include_context 'user_group admin'
+          it_behaves_like 'shows group aggregate stats'
+        end
+
+        context 'querying user is a zooniverse admin' do
+          include_context 'zooniverse admin'
+          it_behaves_like 'shows group aggregate stats'
+        end
+      end
+
+      context 'private_show_agg_and_ind' do
+        include_context 'user group with stats_visibility', 'private_show_agg_and_ind'
+
+        context 'querying user is not a member' do
+          before(:each) {
+            authenticate_with_membership!(classification_user_group, [])
+          }
+
+          it 'does not show group aggregate stats' do
+            get :query, params: { id: classification_user_group.user_group_id.to_s }
+            expect(response.status).to eq(403)
+          end
+        end
+
+        context 'querying user is a group member' do
+          include_context 'user_group member'
+          it_behaves_like 'shows group aggregate stats'
+        end
+
+        context 'querying user is a group admin' do
+          include_context 'user_group admin'
+          it_behaves_like 'shows group aggregate stats'
+        end
+
+        context 'querying user is a zooniverse admin' do
+          include_context 'zooniverse admin'
+          it_behaves_like 'shows group aggregate stats'
+        end
+      end
+
+      context 'private_agg_only' do
+        include_context 'user group with stats_visibility', 'private_agg_only'
+
+        context 'querying user is not a member' do
+          before(:each) {
+            authenticate_with_membership!(classification_user_group, [])
+          }
+
+          it 'does not show group aggregate stats' do
+            get :query, params: { id: classification_user_group.user_group_id.to_s }
+            expect(response.status).to eq(403)
+          end
+        end
+
+        context 'querying user is a group member' do
+          include_context 'user_group member'
+          it_behaves_like 'shows group aggregate stats'
+        end
+
+        context 'querying user is a group admin' do
+          include_context 'user_group admin'
+          it_behaves_like 'shows group aggregate stats'
+        end
+
+        context 'querying user is a zooniverse admin' do
+          include_context 'zooniverse admin'
+          it_behaves_like 'shows group aggregate stats'
+        end
+      end
+    end
+
+    context 'individual_stats_breakdown is true' do
+      context 'public_show_all' do
+        include_context 'user group with stats_visibility', 'public_show_all'
+
+        context 'querying user is not a member' do
+          before(:each) {
+            authenticate_with_membership!(classification_user_group, [])
+          }
+          it_behaves_like 'shows group individual stats breakdown'
+        end
+
+        context 'querying user is a group member' do
+          include_context 'user_group member'
+          it_behaves_like 'shows group individual stats breakdown'
+        end
+
+        context 'querying user is a group admin' do
+          include_context 'user_group admin'
+          it_behaves_like 'shows group individual stats breakdown'
+        end
+
+        context 'querying user is a zooniverse admin' do
+          include_context 'zooniverse admin'
+          it_behaves_like 'shows group individual stats breakdown'
+        end
+      end
+
+      context 'public_agg_show_ind_if_member' do
+        include_context 'user group with stats_visibility', 'public_agg_show_ind_if_member'
+
+        context 'querying user is not a member' do
+          before(:each) {
+            authenticate_with_membership!(classification_user_group, [])
+          }
+
+          it 'does not show individual stats breakdown' do
+            get :query, params: { id: classification_user_group.user_group_id.to_s, individual_stats_breakdown: true }
+            expect(response.status).to eq(403)
+          end
+        end
+
+        context 'querying user is a group member' do
+          include_context 'user_group member'
+          it_behaves_like 'shows group individual stats breakdown'
+        end
+
+        context 'querying user is a group admin' do
+          include_context 'user_group admin'
+          it_behaves_like 'shows group individual stats breakdown'
+        end
+
+        context 'querying user is a zooniverse admin' do
+          include_context 'zooniverse admin'
+          it_behaves_like 'shows group individual stats breakdown'
+        end
+      end
+
+      context 'public_agg_only' do
+        include_context 'user group with stats_visibility', 'public_agg_only'
+
+        context 'querying user is not a member' do
+          before(:each) {
+            authenticate_with_membership!(classification_user_group, [])
+          }
+
+          it 'does not show individual stats breakdown' do
+            get :query, params: { id: classification_user_group.user_group_id.to_s, individual_stats_breakdown: true }
+            expect(response.status).to eq(403)
+          end
+        end
+
+        context 'querying user is a group member' do
+          include_context 'user_group member'
+          it 'does not show individual stats breakdown' do
+            get :query, params: { id: classification_user_group.user_group_id.to_s, individual_stats_breakdown: true }
+            expect(response.status).to eq(403)
+          end
+        end
+
+        context 'querying user is a group admin' do
+          include_context 'user_group admin'
+          it_behaves_like 'shows group individual stats breakdown'
+        end
+
+        context 'querying user is a zooniverse admin' do
+          include_context 'zooniverse admin'
+          it_behaves_like 'shows group individual stats breakdown'
+        end
+      end
+
+      context 'private_show_agg_and_ind' do
+        include_context 'user group with stats_visibility', 'private_show_agg_and_ind'
+
+        context 'querying user is not a member' do
+          before(:each) {
+            authenticate_with_membership!(classification_user_group, [])
+          }
+
+          it 'does not show individual stats breakdown' do
+            get :query, params: { id: classification_user_group.user_group_id.to_s, individual_stats_breakdown: true }
+            expect(response.status).to eq(403)
+          end
+        end
+
+        context 'querying user is a group member' do
+          include_context 'user_group member'
+          it_behaves_like 'shows group individual stats breakdown'
+        end
+
+        context 'querying user is a group admin' do
+          include_context 'user_group admin'
+          it_behaves_like 'shows group individual stats breakdown'
+        end
+
+        context 'querying user is a zooniverse admin' do
+          include_context 'zooniverse admin'
+          it_behaves_like 'shows group individual stats breakdown'
+        end
+      end
+
+      context 'private_agg_only' do
+        include_context 'user group with stats_visibility', 'private_agg_only'
+
+        context 'querying user is not a member' do
+          before(:each) {
+            authenticate_with_membership!(classification_user_group, [])
+          }
+
+          it 'does not show individual stats breakdown' do
+            get :query, params: { id: classification_user_group.user_group_id.to_s, individual_stats_breakdown: true }
+            expect(response.status).to eq(403)
+          end
+        end
+
+        context 'querying user is a group member' do
+          include_context 'user_group member'
+          it 'does not show individual stats breakdown' do
+            get :query, params: { id: classification_user_group.user_group_id.to_s, individual_stats_breakdown: true }
+            expect(response.status).to eq(403)
+          end
+        end
+
+        context 'querying user is a group admin' do
+          include_context 'user_group admin'
+          it_behaves_like 'shows group individual stats breakdown'
+        end
+
+        context 'querying user is a zooniverse admin' do
+          include_context 'zooniverse admin'
+          it_behaves_like 'shows group individual stats breakdown'
+        end
       end
     end
 
@@ -65,5 +392,15 @@ RSpec.describe UserGroupClassificationCountController do
         expect(controller.params[:top_contributors]).to eq(10)
       end
     end
+  end
+
+  def membership(classification_user_group, role_type='group_member')
+    { 'id' => 123,
+      'user_group_id' => classification_user_group.user_group_id, 'user_id' => classification_user_group.user_id,
+      'roles' => [role_type] }
+  end
+
+  def user_group(classification_user_group, stats_visibility)
+    { 'id' => classification_user_group.user_group_id, 'stats_visibility' => stats_visibility }
   end
 end
--- spec/support/authentication_helpers.rb
+++ spec/support/authentication_helpers.rb
@@ -5,6 +5,38 @@ module AuthenticationHelpers
     allow(controller).to receive(:client).and_return(user_client(user_id, is_panoptes_admin))
   end
 
+  def authenticate_with_membership!(classification_user_group, memberships,is_panoptes_admin: false)
+    allow(controller).to receive(:client).and_return(user_client_with_membership(classification_user_group, memberships, is_panoptes_admin))
+  end
+
+  def panoptes_application_client
+    return @panoptes_application_client if @panoptes_application_client
+
+    @panoptes_application_client = instance_double(Panoptes::Client)
+
+    @panoptes_application_client
+  end
+
+  def user_client_with_membership(classification_user_group, memberships, is_panoptes_admin)
+    return @user_client_with_membership if @user_client_with_membership
+
+    me_hash = {
+      'id' => classification_user_group.user_id,
+      'login' => 'login',
+      'display_name' => 'display_name',
+      'admin' => is_panoptes_admin
+    }
+    memberships_url = "/memberships?user_id=#{classification_user_group.user_id}&user_group_id=#{classification_user_group.user_group_id}"
+
+    @user_client_with_membership = double(Panoptes::Client, me: me_hash).tap do |client|
+      allow(client).to receive(:is_a?).and_return(false)
+      allow(client).to receive(:is_a?).with(Panoptes::Client).and_return(true)
+      allow(client).to receive_message_chain(:panoptes, :get).with(memberships_url).and_return('memberships' => memberships)
+    end
+
+    @user_client_with_membership
+  end
+
   def user_client(user_id, is_panoptes_admin)
     return @user_client if @user_client
 
