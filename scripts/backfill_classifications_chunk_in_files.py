73cfae10e7c13956a37d75c087fb21100589c212
yuenmichelle1 <michelle.dano.yuen@gmail.com>
2023-10-17 10:28:37 -0500

    update copy into source

--- scripts/backfill_classifications_chunk_in_files.py
+++ scripts/backfill_classifications_chunk_in_files.py
@@ -46,7 +46,7 @@ print("TIMESCALE START COPY FROM CSV")
 output_file_no = 0
 while output_file_no <= num_files:
     with psycopg.connect(f"host={TIMESCALE_CONNECTION} port={TIMESCALE_PORT} dbname={ERAS_DB} user={ERAS_USER} password={ERAS_PW} sslmode=require") as timescale_db_conn:
-        with timescale_db_conn.cursor(name="timescale_cursor").copy("COPY classification_events FROM STDIN (format csv, delimiter '|', quote '\"')") as timescale_copy:
+        with timescale_db_conn.cursor(name="timescale_cursor").copy("COPY classification_events FROM STDIN DELIMITER ',' CSV HEADER") as timescale_copy:
             timescale_copy.write(open(f"prod_classifications_{output_file_no}.csv").read())
     output_file_no += 1
 
