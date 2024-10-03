4f1e82a273370e60ae0c81312fcb8e9f5cbdfe1a
yuenmichelle1 <michelle.dano.yuen@gmail.com>
2024-10-03 13:33:58 -0500

    initial go on using hourly classifications for workflows

--- app/queries/count_classifications.rb
+++ app/queries/count_classifications.rb
@@ -13,7 +13,19 @@ class CountClassifications
     scoped = @counts
     scoped = filter_by_workflow_id(scoped, params[:workflow_id])
     scoped = filter_by_project_id(scoped, params[:project_id])
-    filter_by_date_range(scoped, params[:start_date], params[:end_date])
+    if params[:workflow_id].present?
+      if end_date_includes_today?(params[:end_date])
+        scoped_upto_yesterday = filter_by_date_range(scoped, params[:start_date], Date.yesterday.to_s)
+
+        puts scoped_upto_yesterday
+        scoped = append_to_scoped(scoped_upto_yesterday, params[:workflow_id], params[:period])
+      else
+        scoped = filter_by_date_range(scoped, params[:start_date], params[:end_date])
+      end
+    else
+      scoped = filter_by_date_range(scoped, params[:start_date], params[:end_date])
+    end
+    return scoped
   end
 
   private
@@ -22,12 +34,54 @@ class CountClassifications
     relation.select(select_and_time_bucket_by(period, 'classification')).group('period').order('period')
   end
 
-  def end_date_includes_today?(params)
-    includes_today = true
-    if params[:end_date]
-      end_date = Date.parse(params[:end_date])
-      includes_today = end_date >= Date.today
+  def append_to_scoped(scoped_upto_yesterday, workflow_id, period)
+    todays_classifications = current_date_workflow_classifications(workflow_id)
+    most_recent_period_from_scoped = scoped_upto_yesterday[-1].period&.to_date
+    most_recent_count = scoped_upto_yesterday[-1].count
+    case period
+    when 'day'
+      scoped_upto_yesterday + todays_classifications
+    when 'week'
+      if (Date.today - most_recent_period_from_scoped).to_i < 7
+        add_todays_counts_to_recent_period_counts(scoped_upto_yesterday, todays_classifications)
+      else
+        scoped_upto_yesterday + todays_classifications
+      end
+    when 'month'
+      if (Date.today.month == most_recent_period_from_scoped.month)
+        add_todays_counts_to_recent_period_counts(scoped_upto_yesterday, todays_classifications)
+      else
+        scoped_upto_yesterday + todays_classifications
+      end
+    when 'year'
+      if (Date.today.year == most_recent_period_from_scoped.year)
+        add_todays_counts_to_recent_period_counts(scoped_upto_yesterday, todays_classifications)
+      else
+        scoped_upto_yesterday + todays_classifications
+      end
     end
+  end
+
+  def add_todays_counts_to_recent_period_counts(scoped_upto_yesterday, todays_classifications)
+    current_period_counts = scoped_upto_yesterday[-1].count + todays_classifications[0].count
+    scoped_upto_yesterday[-1].count = current_period_counts
+    scoped_upto_yesterday
+  end
+
+  def current_date_workflow_classifications(workflow_id)
+    current_day_str = Date.today.to_s
+    current_hourly_classifications = ClassificationCounts::HourlyWorkflowClassificationCount.select("time_bucket('1 day', hour) AS period, SUM(classification_count)::integer AS count").group('period').order('period').where("hour >= '#{current_day_str}'")
+    filter_by_workflow_id(current_hourly_classifications, workflow_id)
+  end
+
+  # if period is day append today's result as a result
+  # if period is week/month or year first check if today is in the week or month or year
+  # if it is, add the count to the last count
+  # if it isn't add a new entry to result
+
+  def end_date_includes_today?(end_date)
+    includes_today = true
+    includes_today = Date.parse(end_date) >= Date.today if end_date.present?
     return includes_today
   end
 
