77cb2629d17d3c6bf81ed1f3db2bc7d75fbb8cdd
yuenmichelle1 <michelle.dano.yuen@gmail.com>
2024-10-03 16:53:27 -0500

    Update count_classifications.rb

--- app/queries/count_classifications.rb
+++ app/queries/count_classifications.rb
@@ -16,7 +16,7 @@ class CountClassifications
     if params[:workflow_id].present?
       if end_date_includes_today?(params[:end_date])
         scoped_upto_yesterday = filter_by_date_range(scoped, params[:start_date], Date.yesterday.to_s)
-        scoped = append_to_scoped(scoped_upto_yesterday, params[:workflow_id], params[:period])
+        scoped = include_today_to_scoped(scoped_upto_yesterday, params[:workflow_id], params[:period])
       else
         scoped = filter_by_date_range(scoped, params[:start_date], params[:end_date])
       end
@@ -32,38 +32,38 @@ class CountClassifications
     relation.select(select_and_time_bucket_by(period, 'classification')).group('period').order('period')
   end
 
-  def append_to_scoped(scoped_upto_yesterday, workflow_id, period)
+  def include_today_to_scoped(scoped_upto_yesterday, workflow_id, period)
     todays_classifications = current_date_workflow_classifications(workflow_id)
-    most_recent_period_from_scoped = scoped_upto_yesterday[-1].period&.to_date
+    most_recent_date_from_scoped = scoped_upto_yesterday[-1].period&.to_date
     most_recent_count = scoped_upto_yesterday[-1].count
-    case period
-    when 'day'
-      scoped_upto_yesterday + todays_classifications
-    when 'week'
-      if (Date.today - most_recent_period_from_scoped).to_i < 7
-        add_todays_counts_to_recent_period_counts(scoped_upto_yesterday, todays_classifications)
-      else
-        scoped_upto_yesterday + todays_classifications
-      end
-    when 'month'
-      if (Date.today.month == most_recent_period_from_scoped.month)
-        add_todays_counts_to_recent_period_counts(scoped_upto_yesterday, todays_classifications)
-      else
-        scoped_upto_yesterday + todays_classifications
-      end
-    when 'year'
-      if (Date.today.year == most_recent_period_from_scoped.year)
-        add_todays_counts_to_recent_period_counts(scoped_upto_yesterday, todays_classifications)
-      else
-        scoped_upto_yesterday + todays_classifications
-      end
+    if is_today_part_of_recent_period?(most_recent_date_from_scoped, period)
+      add_todays_counts_to_recent_period_counts(scoped_upto_yesterday, todays_classifications)
+    else
+      append_today_to_scoped(scoped_upto_yesterday, todays_classifications)
     end
   end
 
-  def add_todays_counts_to_recent_period_counts(scoped_upto_yesterday, todays_classifications)
-    current_period_counts = scoped_upto_yesterday[-1].count + todays_classifications[0].count
-    scoped_upto_yesterday[-1].count = current_period_counts
-    scoped_upto_yesterday
+  def is_today_part_of_recent_period?(most_recent_date, period)
+    case period
+    when 'day'
+      false
+    when 'week'
+      (Date.today - most_recent_date).to_i < 7
+    when 'month'
+      Date.today.month == most_recent_date.month
+    when 'year'
+      Date.today.year == most_recent_date.year
+    end
+  end
+
+  def append_today_to_scoped(count_records_up_to_yesterday, todays_count)
+    count_records_up_to_yesterday + todays_count
+  end
+
+  def add_todays_counts_to_recent_period_counts(count_records_up_to_yesterday, todays_counts
+    current_period_counts = count_records_up_to_yesterday[-1].count + todays_count[0].count
+    count_records_up_to_yesterday[-1].count = current_period_counts
+    count_records_up_to_yesterday
   end
 
   def current_date_workflow_classifications(workflow_id)
