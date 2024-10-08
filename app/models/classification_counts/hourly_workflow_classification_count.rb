789b5553eac5bbb77aa9695dcd18ce214cb41819
yuenmichelle1 <michelle.dano.yuen@gmail.com>
2024-10-07 23:37:40 -0500

    Update hourly_workflow_classification_count.rb

--- app/models/classification_counts/hourly_workflow_classification_count.rb
+++ app/models/classification_counts/hourly_workflow_classification_count.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
 module ClassificationCounts
-    class HourlyWorkflowClassificationCount < ApplicationRecord
-      self.table_name = 'hourly_classification_count_per_workflow'
-      attribute :classification_count, :integer
+  class HourlyWorkflowClassificationCount < ApplicationRecord
+    self.table_name = 'hourly_classification_count_per_workflow'
+    attribute :classification_count, :integer
 
-      def readonly?
-        true
-      end
+    def readonly?
+      true
     end
   end
+end
