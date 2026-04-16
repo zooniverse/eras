e205717074652b3ab39495385f975abdb063ddfa
yuenmichelle1 <michelle.dano.yuen@gmail.com>
2026-04-16 07:59:07 -0500

    Create updated materialized views to include project session time
    Create updated materialized views to include project session time.
    
    In order the next steps after merge, is to:
    - drop in replace DailyProjectClassificationCount with DailyProjectClassificationCountAndTime
    - Merge, Test, and Deploy
    - then DROP DailyProjectClassificationCount CAgg to free up space (24 kB)
    
    - Thn work on Project Count Stats Efficiencies
    (Similar to workflow, we Alter daily project count and time to a Non-Real Time Aggregate)
    - Update logic so that anything that queries for "NOW()" will query from the real-time hourly aggregate appended with the appropriate non-realtime aggregate

---
+++ db/migrate/20260416033148_add_refresh_policy_for_hourly_project_count_and_time.rb
@@ -0,0 +1,14 @@
+class AddRefreshPolicyForHourlyProjectCountAndTime < ActiveRecord::Migration[7.0]
+  disable_ddl_transaction!
+  def up
+    execute <<~SQL
+      SELECT add_continuous_aggregate_policy('hourly_classification_count_and_time_per_project',start_offset => INTERVAL '5 days', end_offset => INTERVAL '30 minutes', schedule_interval => INTERVAL '1 h');
+    SQL
+  end
+
+  def down
+    execute <<~SQL
+      SELECT remove_continuous_aggregate_policy('hourly_classification_count_and_time_per_project');
+    SQL
+  end
+end
---
+++ db/migrate/20260416033213_add_retention_policy_for_hourly_project_count_and_time.rb
@@ -0,0 +1,14 @@
+class AddRetentionPolicyForHourlyProjectCountAndTime < ActiveRecord::Migration[7.0]
+  disable_ddl_transaction!
+  def up
+    execute <<~SQL
+      SELECT add_retention_policy('hourly_classification_count_and_time_per_project', drop_after => INTERVAL '3 days');
+    SQL
+  end
+
+  def down
+    execute <<~SQL
+      SELECT remove_retention_policy('hourly_classification_count_and_time_per_project');
+    SQL
+  end
+end
