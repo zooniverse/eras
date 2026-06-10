07eeb286d5ae5fe078d480921c3a34237076ebd9
yuenmichelle1 <michelle@zooniverse.org>
2026-06-10 11:59:59 -0500

    update script and move hash creations to methods for some cleanup

--- scripts/spurious_classifications_alerter.rb
+++ scripts/spurious_classifications_alerter.rb
@@ -22,45 +22,58 @@ def format_report_for_slack(hash_of_arrays)
   hash_of_arrays.map { |k, v| "<https://www.zooniverse.org/lab/#{k}|Project #{k}>: #{v}" }.join("\n")
 end
 
-puts 'Querying diffs to flag potential affected projects...'
-projects_weekly_classifications_history = ActiveRecord::Base.connection.exec_query("SELECT record1.day as day1, record2.day as day_compare, record1.project_id, record2.project_id, record1.classification_count as day1_count, record2.classification_count as day_compare_count,abs(cast(record2.classification_count - record1.classification_count as float) / record1.classification_count) * 100 as percentage_diff, abs(cast(record2.classification_count - record1.classification_count as float) / extract(day from record2.day - record1.day)) as classification_rate
-FROM
-    daily_classification_count_and_time_per_project AS record1
-INNER JOIN
-    daily_classification_count_and_time_per_project AS record2 ON record1.project_id = record2.project_id
-WHERE
-    record1.classification_count IS NOT NULL AND record2.classification_count IS NOT NULL and record1.day < record2.day and record1.day >= (CURRENT_DATE - INTERVAL '7 days') and record2.day >= CURRENT_DATE - INTERVAL '7 days' and record2.day < CURRENT_DATE and record1.classification_count > 1000 and record2.classification_count > 1000 order by classification_rate desc;")
+def projects_weekly_classifications_history
+  puts 'Querying diffs to flag potential affected projects...'
 
-flagged_project_id_to_high_classifying_dates = Hash.new { |h, k| h[k] = [] }
-projects_weekly_classifications_history.each do |proj_history|
-  next unless proj_history['classification_rate'] >= PROJECT_SPURIOUS_CLASSIFICATION_RATE_LOWER_BOUND && proj_history['percentage_diff'] >= PERCENTAGE_DIFF_THRESHOLD
+  ActiveRecord::Base.connection.exec_query("SELECT record1.day as day1, record2.day as day_compare, record1.project_id, record2.project_id, record1.classification_count as day1_count, record2.classification_count as day_compare_count,abs(cast(record2.classification_count - record1.classification_count as float) / record1.classification_count) * 100 as percentage_diff, abs(cast(record2.classification_count - record1.classification_count as float) / extract(day from record2.day - record1.day)) as classification_rate
+  FROM
+      daily_classification_count_and_time_per_project AS record1
+  INNER JOIN
+      daily_classification_count_and_time_per_project AS record2 ON record1.project_id = record2.project_id
+  WHERE
+      record1.classification_count IS NOT NULL AND record2.classification_count IS NOT NULL and record1.day < record2.day and record1.day >= (CURRENT_DATE - INTERVAL '7 days') and record2.day >= CURRENT_DATE - INTERVAL '7 days' and record2.day < CURRENT_DATE and record1.classification_count > 1000 and record2.classification_count > 1000 order by classification_rate desc;")
+end
 
-  if proj_history['day1_count'] > proj_history['day_compare_count']
-    flagged_project_id_to_high_classifying_dates[proj_history['project_id']] << proj_history['day1'].strftime('%Y-%m-%d')
-  elsif proj_history['day_compare_count'] > proj_history['day1_count']
-    flagged_project_id_to_high_classifying_dates[proj_history['project_id']] << proj_history['day_compare'].strftime('%Y-%m-%d')
+def flagged_projects_to_high_classifying_dates
+  projects_weekly_classifications_history.each_with_object(Hash.new { |h, k| h[k] = [] }) do |proj_history, projects_to_high_dates|
+    next unless proj_history['classification_rate'] >= PROJECT_SPURIOUS_CLASSIFICATION_RATE_LOWER_BOUND && proj_history['percentage_diff'] >= PERCENTAGE_DIFF_THRESHOLD
+
+    date =
+      if proj_history['day1_count'] > proj_history['day_compare_count']
+        proj_history['day1']
+      elsif proj_history['day_compare_count'] > proj_history['day1_count']
+        proj_history['day_compare']
+      end
+
+    projects_to_high_dates[proj_history['project_id']] << date.strftime('%Y-%m-%d') if date
+  end.then { |flagged_projs_with_dates_hash| normalize_hash_values(flagged_projs_with_dates_hash) }
+end
+
+def user_rates_for_project(proj_id, dates)
+  ActiveRecord::Base.connection.exec_query('SELECT *, cast(classification_count as float) / total_session_time as rate from daily_user_classification_count_and_time_per_project where project_id = $1 and day = ANY($2) order by classification_count desc', 'SQL', [proj_id, "{#{dates.join(',')}}"])
+end
+
+def flagged_users(projects_to_high_classified_dates)
+  tier_one = Hash.new { |h, k| h[k] = [] }
+  tier_two = Hash.new { |h, k| h[k] = [] }
+
+  projects_to_high_classified_dates.each do |project_id, dates|
+    user_rates_for_project(project_id, dates).each do |user_rate|
+      classification_count = user_rate['classification_count']
+      tier_one[project_id] << user_rate['user_id'] if classification_count >= USER_CLASSIFICATION_COUNT_THRESHOLD_TIER_ONE && classification_count <= USER_CLASSIFICATION_COUNT_THRESHOLD_TIER_TWO
+
+      tier_two[project_id] << user_rate['user_id'] if classification_count >= USER_CLASSIFICATION_COUNT_THRESHOLD_TIER_TWO
+    end
   end
+
+  [normalize_hash_values(tier_one), normalize_hash_values(tier_two)]
 end
 
 puts 'Potential Affected Project IDs...'
-flagged_project_id_to_high_classifying_dates = normalize_hash_values(flagged_project_id_to_high_classifying_dates)
+flagged_projects = flagged_projects_to_high_classifying_dates
 
 puts 'Finding Potential Spurious Classifiers for each Project...'
-
-flagged_project_id_to_high_classifiers_tier_one = Hash.new { |h, k| h[k] = [] }
-flagged_project_id_to_high_classifiers_tier_two = Hash.new { |h, k| h[k] = [] }
-flagged_project_id_to_high_classifying_dates.each do |proj_id, dates|
-  user_rates_for_proj = ActiveRecord::Base.connection.exec_query('SELECT *, cast(classification_count as float) / total_session_time as rate from daily_user_classification_count_and_time_per_project where project_id = $1 and day = ANY($2) order by classification_count desc', 'SQL', [proj_id, "{#{dates.join(',')}}"])
-
-  user_rates_for_proj.each do |user_rate|
-    flagged_project_id_to_high_classifiers_tier_one[proj_id] << user_rate['user_id'] if user_rate['classification_count'] >= USER_CLASSIFICATION_COUNT_THRESHOLD_TIER_ONE && user_rate['classification_count'] <= USER_CLASSIFICATION_COUNT_THRESHOLD_TIER_TWO
-
-    flagged_project_id_to_high_classifiers_tier_two[proj_id] << user_rate['user_id'] if user_rate['classification_count'] > USER_CLASSIFICATION_COUNT_THRESHOLD_TIER_TWO
-  end
-end
-
-flagged_project_id_to_high_classifiers_tier_one = normalize_hash_values(flagged_project_id_to_high_classifiers_tier_one)
-flagged_project_id_to_high_classifiers_tier_two = normalize_hash_values(flagged_project_id_to_high_classifiers_tier_two)
+flagged_project_id_to_high_classifiers_tier_one, flagged_project_id_to_high_classifiers_tier_two = flagged_users(flagged_projects)
 
 webhook_url = Rails.application.credentials.dig(:slack, :webhook_url)
 
@@ -91,7 +104,7 @@ message = {
       type: 'section',
       text: {
         type: 'mrkdwn',
-        text: format_report_for_slack(flagged_project_id_to_high_classifying_dates).presence || 'None'
+        text: format_report_for_slack(flagged_projects).presence || 'None'
       }
     },
     {
